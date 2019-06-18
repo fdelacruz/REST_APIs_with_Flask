ec43d08f7747ff755276d4e7f03a472ef45e8fb4
Francisco De La Cruz <helloquico@gmail.com>
2019-06-18 08:02:07 -0400

    First Flask-RESTful app

--- app.py
+++ app.py
@@ -1,68 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-stores = [
-    {
-        'name': 'my wonderful store',
-        'items': [
-            {
-                'name': 'my item',
-                'price': 15.99
-            }
-         ]
-    }
-]
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
 
-# POST /store data: {name:}
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-            'name': request_data['name'],
-            'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-
-
-# GET /store/<string:name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    # iterate over stores
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-
-
-# GET /store
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-# POST /store/<string:name>/item {name:, price:}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                    'name': request_data['name'],
-                    'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found'})
-
-
-# GET /store/<string:name>/item data: {name :}
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
-
+api.add_resource(Student, '/student/<string:name>')
 
 app.run(debug=True)
