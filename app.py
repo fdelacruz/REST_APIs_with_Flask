07846706d79f3fa2b6b14706aa5137803e5cbcf4
Francisco De La Cruz <helloquico@gmail.com>
2019-06-20 08:55:32 -0400

    Optimise final code and request parsing

--- app.py
+++ app.py
@@ -13,22 +13,23 @@ jwt = JWT(app, authenticate, identity) # /auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     # @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']== name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-            type=float,
-            required=True,
-            help='This field cannot be left blank!'
-        )
-        data = parser.parse_args()
-
         if next(filter(lambda x: x['name']== name, items), None):
             return {'message': 'An item with name "{}" already exists.'.format(name)}, 400 
+
+        data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
         items.append(item)
@@ -41,20 +42,16 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-            type=float,
-            required=True,
-            help='This field cannot be left blank!'
-        )
-        data = parser.parse_args()
-
         item = next(filter(lambda x: x['name']== name, items), None)
+
+        data = Item.parser.parse_args()
+
         if item is None:
             item = {'name': name, 'price': data['price']}
             items.append(item)
         else:
             item.update(data)
+
 
         return item
 
